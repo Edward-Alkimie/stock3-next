bb799ab98d774e4747154a1f00ecfb83e6d234cc
kevin <wangk971119@gmail.com>
2023-05-18 10:39:51 -0400

    WIP fetch hook

--- components/Data.tsx
+++ components/Data.tsx
@@ -1,11 +1,13 @@
 
+import { useEffect, useState } from "react"
+
 export async function fetchStockRatio(){
-    const ratioFinn = "http://localhost:3000/api/chartInfo/read"
-    try {
-        fetch(ratioFinn)
-        .then(response => response.json())
-        .then(responseJson => console.log(responseJson))
-    } catch(error) {
-        console.error("There has been problem with the fetch operation:", error)
-    }
+    const userChartLayout = "http://localhost:3000/api/chartInfo/read"
+    const [userChart, setUserChart] =useState("")
+    useEffect(() =>{
+        fetch(userChartLayout)
+            .then(response => response.json())
+            .then(jsonFile => setUserChart(jsonFile))
+            .catch(error => console.error("There has been problem with the fetch operation:", error))
+    },[])
 }
