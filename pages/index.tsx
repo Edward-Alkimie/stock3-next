ea3cd9f5bce8cdb09ad1fc1ba0f834a81e626d8b
kevin <wangk971119@gmail.com>
2023-06-08 15:59:37 -0400

    WIP provider

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,15 +1,19 @@
 import { Inter } from 'next/font/google'
 import  Chart  from '../components/Chart'
 import FetchStockRatio  from '@/components/Data'
+import React, { createContext, useState } from "react";
+import ChartDataConext from '@/components/ChartDataContext'
 
 const inter = Inter({ subsets: ['latin'] })
+
 
 export default function Home() {
   return (
     <main>
+      <ChartDataConext.Provider value=UserContext={}>
       <Chart/>
       <FetchStockRatio/>
-
+      <>
     </main>
   )
 }
